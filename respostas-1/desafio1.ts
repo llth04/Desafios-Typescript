dda6b78ac2be87f6be288cd958aa9efd10f38291
unknown <isabelafatimadelimagx@gmail.com>
2022-06-30 16:03:39 -0300

    first commit

---
+++ respostas-1/desafio1.ts
@@ -0,0 +1,9 @@
+const funcionario2: {codigo: number, nome: string} = {
+    codigo: 10,
+    nome: 'joao'
+}
+
+interface funcionario{
+    codigo: number;
+    nome: string;
+}
---
+++ respostas-1/desafio3.ts
@@ -0,0 +1,35 @@
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo');
+
+let saldoTotal = 0
+
+limparSaldo()
+
+function somarSaldo(soma: number) {
+    if(campoSaldo){
+        saldoTotal += soma
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+function limparCampoSoma(){
+    soma.value= "";
+}
+
+function limparSaldo() {
+    if(campoSaldo){
+        saldoTotal=0;
+        campoSaldo.innerHTML=saldoTotal.toString();
+    }
+}
+
+if(botaoAtualizar){
+botaoAtualizar.addEventListener('click', function () {
+    somarSaldo(Number(soma.value));
+});}
+if(botaoLimpar){
+botaoLimpar.addEventListener('click', function () {
+   limparSaldo();
+});}
